b1f3c2abbe60b4c0ab2aef675011b1f1812e74f2
Vyecheslav Borisov <vyachic007@gmail.com>
2025-12-07 19:14:02 +0300

    SENLA-8: Реализован модуль di-module с паттерном Dependency Injection

---
+++ task-8/SENLA-8/di-module/src/main/java/by/slava_borisov/di/DIUtil.java
@@ -0,0 +1,18 @@
+package by.slava_borisov.di;
+
+public class DIUtil {
+    private static final DIContainer container = new DIContainer();
+
+    public static <T> void register(Class<T> clazz, T bean) {
+        container.register(clazz, bean);
+    }
+
+    public static <T> T get(Class<T> clazz) {
+        return container.get(clazz);
+    }
+
+    public static void injectDependencies(Object target) throws IllegalAccessException {
+        container.injectDependencies(target);
+    }
+}
+
