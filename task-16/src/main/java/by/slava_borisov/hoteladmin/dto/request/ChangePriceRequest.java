0b7dff05ad2808b8dc5556a6a62c282838a428a5
Vyacheslav Borisov <vyachic007@gmail.com>
2026-02-19 00:13:52 +0300

    SENLA-16: добавлены DTO для запросов REST API

---
+++ task-16/src/main/java/by/slava_borisov/hoteladmin/dto/request/ChangePriceRequest.java
@@ -0,0 +1,5 @@
+package by.slava_borisov.hoteladmin.dto.request;
+
+public record ChangePriceRequest(
+        double newPrice
+) { }
---
+++ task-16/src/main/java/by/slava_borisov/hoteladmin/dto/request/RoomStatusRequest.java
@@ -0,0 +1,7 @@
+package by.slava_borisov.hoteladmin.dto.request;
+
+import by.slava_borisov.hoteladmin.model.RoomStatus;
+
+public record RoomStatusRequest(
+        RoomStatus status
+) { }
