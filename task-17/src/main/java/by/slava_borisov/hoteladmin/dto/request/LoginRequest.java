56042496bbe6727e3f5c1ea18b7f2cd34baf42a8
Vyacheslav Borisov <vyachic007@gmail.com>
2026-03-15 21:02:47 +0300

    SENLA-19: добавил LoginRequest и LoginResponse DTO

---
+++ task-17/src/main/java/by/slava_borisov/hoteladmin/dto/request/LoginRequest.java
@@ -0,0 +1,6 @@
+package by.slava_borisov.hoteladmin.dto.request;
+
+public record LoginRequest(
+        String username,
+        String password
+) { }
---
+++ task-17/src/main/java/by/slava_borisov/hoteladmin/dto/response/LoginResponse.java
@@ -0,0 +1,5 @@
+package by.slava_borisov.hoteladmin.dto.response;
+
+public record LoginResponse(
+        String token
+) { }
