d943b2d1daa18f5abfbdbfb9434984f330ee8865
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-04 14:55:39 +0300

    SENLA-17: исправил импорты в entity по требованиям Checkstyle

--- task-17/src/main/java/by/slava_borisov/hoteladmin/model/Amenity.java
+++ task-17/src/main/java/by/slava_borisov/hoteladmin/model/Amenity.java
@@ -1,6 +1,12 @@
 package by.slava_borisov.hoteladmin.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
