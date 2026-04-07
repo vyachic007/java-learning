ae13b2a82c4a088a366fddd9ca3e297c16b0b32b
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-07 22:29:48 +0300

    SENLA-17: заменил double и String на BigDecimal и LocalDate в request DTO

--- task-17/src/main/java/by/slava_borisov/hoteladmin/dto/request/PriceCalculationRequest.java
+++ task-17/src/main/java/by/slava_borisov/hoteladmin/dto/request/PriceCalculationRequest.java
@@ -1,7 +1,9 @@
 package by.slava_borisov.hoteladmin.dto.request;
 
+import java.time.LocalDate;
+
 public record PriceCalculationRequest(
         Long roomId,
-        String checkInDate,
-        String checkOutDate
+        LocalDate checkInDate,
+        LocalDate checkOutDate
 ) { }
